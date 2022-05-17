64085477eeebea9a1869341e856a8b65fe992312
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-17 19:48:47 +0800

    First commit

---
+++ app/src/main/java/com/mycompany/todoapp/Todo.java
@@ -0,0 +1,12 @@
+package com.mycompany.todoapp;
+
+public class Todo {
+    String id, title, description, date, status;
+	Todo(String id, String title, String description, String date, String status){
+		this.id=id;
+		this.title=title;
+		this.description=description;
+		this.date=date;
+		this.status=status;
+	}
+}
